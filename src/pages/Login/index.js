c5aa9e5661405132837e5e145b978fab86eebfc9
pyragnis <enzosabbatorsi@outlook.fr>
2023-12-15 17:07:15 +0100

    modif player music

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -30,7 +30,7 @@ const Login = () => {
     <LoginPage>
       <h1 style={{ color: '#ffd700' }}>Yellowfy</h1>
       <Button onClick={handleLogin}>Connexion sur Yellowfy</Button>
-      <MusicPlayer trackUrl={'https://yellowfy.s3.eu-west-3.amazonaws.com/8ae85c09e3a9f8cf37e1dacbe2613f70d4d773c4a719a2bccdde19e9a16e6233.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIA4XITKHRTD2OIYOFL%2F20231207%2Feu-west-3%2Fs3%2Faws4_request&X-Amz-Date=20231207T151648Z&X-Amz-Expires=518400&X-Amz-Signature=a299b58bd9e5e2285f65ff883b3b48ff0a99cc9fda77623ce871028f7b292fb1&X-Amz-SignedHeaders=host&x-id=GetObject'} imageUrl={'https://www.powertrafic.fr/wp-content/uploads/2023/04/image-ia-exemple.png'} />
+      <MusicPlayer trackUrl={'https://yellowfy.s3.eu-west-3.amazonaws.com/77185e24477843482bb1b922423b49f6eef4b1f103acafbd01f7dc27166a560b.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIA4XITKHRTD2OIYOFL%2F20231215%2Feu-west-3%2Fs3%2Faws4_request&X-Amz-Date=20231215T081919Z&X-Amz-Expires=518400&X-Amz-Signature=024f66f04c3affccd67cfdaa45c78e9d070c1225665f472257a16e556df96ada&X-Amz-SignedHeaders=host&x-id=GetObject'} imageUrl={'https://yellowfy.s3.eu-west-3.amazonaws.com/f1ff48af71be445a621b4b3e1c273c4156331ea5ec51b09073becf37d1e80dfc.avif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIA4XITKHRTD2OIYOFL%2F20231215%2Feu-west-3%2Fs3%2Faws4_request&X-Amz-Date=20231215T081919Z&X-Amz-Expires=518400&X-Amz-Signature=af08830b408bf75f0ad1d95016f4c867a7dd148488f439dd81227127264ee404&X-Amz-SignedHeaders=host&x-id=GetObject'} />
 
     </LoginPage>
   );
