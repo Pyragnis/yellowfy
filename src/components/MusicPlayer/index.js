27c20e7b82a6be4ecc4f23c0856552ad0feb772a
pyragnis <enzosabbatorsi@outlook.fr>
2023-12-17 18:21:57 +0100

    update

--- src/components/MusicPlayer/index.js
+++ src/components/MusicPlayer/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import ReactH5AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
@@ -11,7 +11,8 @@ const AudioPlayerWrapper = styled.div`
   max-width: 600px;
   margin: auto;
   text-align: center;
-  border-radius:33px;   
+  border-redius:33px;
+
 
   img {
     width: 100%;
@@ -23,8 +24,7 @@ const AudioPlayerWrapper = styled.div`
     background-color: yellow;
     color: black;
     border: none;
-    
-    
+    padding:2px;
     cursor: pointer;
     border-radius: 4px;
     font-size: 16px;
@@ -34,62 +34,114 @@ const AudioPlayerWrapper = styled.div`
     background-color: black;
     color: yellow;
   }
+
+`;
+const CustomButton = styled.button`
+background-color: yellow;
+color: black;
+border: none;
+padding:2px;
+cursor: pointer;
+border-radius: 4px;
+font-size: 16px;
+padding: 10px 20px;
+  margin: 10px;
+  
 `;
 
 const AudioPlayer = ({ songs }) => {
-  const [currentSongIndex, setCurrentSongIndex] = useState(0);
-  const audioPlayer = useRef(null);
-
-  const handleSongChange = (newIndex) => {
-    setCurrentSongIndex(newIndex);
-    audioPlayer.current.audio.current.pause();
-  };
-
-  const handleNextSong = () => {
-    const newIndex = (currentSongIndex + 1) % songs.length;
-    handleSongChange(newIndex);
-  };
-
-  const handlePrevSong = () => {
-    const newIndex = (currentSongIndex - 1 + songs.length) % songs.length;
-    handleSongChange(newIndex);
-  };
-
-  const handleFullScreen = () => {
-    const player = audioPlayer.current.audio.current;
-    if (player.requestFullscreen) {
-      player.requestFullscreen();
-    } else if (player.mozRequestFullScreen) {
-      player.mozRequestFullScreen();
-    } else if (player.webkitRequestFullscreen) {
-      player.webkitRequestFullscreen();
-    } else if (player.msRequestFullscreen) {
-      player.msRequestFullscreen();
-    }
-  };
-
-  const currentSong = songs[currentSongIndex];
-
-  return (
-    <AudioPlayerWrapper>
+    const [currentSongIndex, setCurrentSongIndex] = useState(0);
+    const [isPlayerReady, setIsPlayerReady] = useState(false);
+    const [isFullScreen, setIsFullScreen] = useState(false);
+    const audioPlayer = useRef(null);
+  
+    const handleSongChange = (newIndex) => {
+      setCurrentSongIndex(newIndex);
+    };
+  
+    const handleNextSong = () => {
+      const newIndex = (currentSongIndex + 1) % songs.length;
+      handleSongChange(newIndex);
+    };
+  
+    const handlePrevSong = () => {
+      const newIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+      handleSongChange(newIndex);
+    };
+  
+    const handleFullScreen = () => {
+      const player = audioPlayer.current.audio.current;
+  
+      if (!isFullScreen) {
+        if (player.requestFullscreen) {
+          player.requestFullscreen();
+        } else if (player.mozRequestFullScreen) {
+          player.mozRequestFullScreen();
+        } else if (player.webkitRequestFullscreen) {
+          player.webkitRequestFullscreen();
+        } else if (player.msRequestFullscreen) {
+          player.msRequestFullscreen();
+        }
+  
+        // Ajoutez la vérification pour le verrouillage du clavier en mode plein écran
+        document.addEventListener('fullscreenchange', handleFullScreenChange);
+      } else {
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+          document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+          document.msExitFullscreen();
+        }
+      }
+  
+      setIsFullScreen(!isFullScreen);
+    };
+  
+    const handleFullScreenChange = () => {
+      if (!document.fullscreenElement) {
+        document.removeEventListener('fullscreenchange', handleFullScreenChange);
+      }
+    };
+  
+    const currentSong = songs[currentSongIndex];
+  
+    useEffect(() => {
+      // Reset player position when the song changes
+      if (isPlayerReady) {
+        audioPlayer.current.audio.current.currentTime = 0;
+        audioPlayer.current.audio.current.play();
+      }
+    }, [currentSongIndex, isPlayerReady]);
+  
+    return (
+      <AudioPlayerWrapper>
         <img src={currentSong.imageUrl} alt={currentSong.title} />
-      <ReactH5AudioPlayer
-        ref={audioPlayer}
-        autoPlay
-        src={currentSong.audioUrl}
-        onEnded={handleNextSong}
-        customControlsSection={['MAIN_CONTROLS', 'VOLUME_CONTROLS']}
-        header={`Now Playing: ${currentSong.title}`}
-        layout="stacked-reverse"
-      />
-      <div>
-        <button onClick={handlePrevSong}>Previous</button>
-        <button onClick={handleNextSong}>Next</button>
-        <button onClick={handleFullScreen}>Fullscreen</button>
-      </div>
-      
-    </AudioPlayerWrapper>
-  );
-};
-
-export default AudioPlayer;
+        <ReactH5AudioPlayer
+          ref={audioPlayer}
+          autoPlay
+          src={currentSong.audioUrl}
+          onEnded={handleNextSong}
+          customControlsSection={['MAIN_CONTROLS', 'VOLUME_CONTROLS']}
+          header={`Now Playing: ${currentSong.title}`}
+          layout="stacked-reverse"
+          onPlay={() => setIsPlayerReady(true)}
+        />
+        <div>
+          <CustomButton onClick={handlePrevSong}>
+            Previous
+          </CustomButton>
+          <CustomButton onClick={handleNextSong}>
+            Next
+          </CustomButton>
+          <CustomButton onClick={handleFullScreen}>
+            Fullscreen
+          </CustomButton>
+        </div>
+      </AudioPlayerWrapper>
+    );
+  };
+  
+  export default AudioPlayer;
